acf05c4feffab4cdb246b0c1ac795dd05f4db88b
pradeep <k.pradeep0491@gmail.com>
2024-05-18 22:23:23 +0530

    changes

---
+++ kafka-javaguides/producerandconsumerJson/src/main/java/com/test/springboot/kafka/JsonKafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.test.springboot.kafka;
+
+import com.test.springboot.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+    private static final Logger log = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+    @KafkaListener(topics = "javaguides_json", groupId = "myGroup")
+    public void consume(User user) {
+        log.info(String.format("Json Message Received->%s", user.toString()));
+    }
+}
