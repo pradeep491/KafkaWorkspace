b6cb33911737e223f3af1a3b8d7d09861fee99da
pradeep <k.pradeep0491@gmail.com>
2024-06-19 11:07:13 +0530

    changes

---
+++ kafka-javaguides/spring-boot-kafka-EventDriven/base-domains/src/main/java/com/test/basedomains/dto/Order.java
@@ -0,0 +1,15 @@
+package com.test.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private String orderId;
+    private String orderName;
+    private int quantity;
+    private double price;
+}
---
+++ kafka-javaguides/spring-boot-kafka-EventDriven/order-service/src/main/java/com/test/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,4 @@
+package com.test.orderservice.kafka;
+
+public class OrderProducer {
+}
